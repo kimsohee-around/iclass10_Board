993703a77f132ab98532e4a2b36795e294d242c1
kimsewhee <koreait.2021@gmail.com>
2023-01-31 23:52:47 +0900

    #6 추가된 dao 메소드 활용하여 테스트 - insert 수정 - list 추가

--- test/org/iclass/BoardTest.java
+++ test/org/iclass/BoardTest.java
@@ -2,6 +2,8 @@ package org.iclass;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.iclass.dao.CommunityCommentsDao;
 import org.iclass.dao.CommunityDao;
 import org.iclass.vo.Community;
@@ -12,16 +14,25 @@ class BoardTest {
 	CommunityDao communityDao = CommunityDao.getInstance();
 	CommunityCommentsDao commentsDao = CommunityCommentsDao.getInstance();
 	
-	@DisplayName("메인글 쓰기 insert")
-	@Test
+	@DisplayName("메인글 쓰기 insert 에 저장된 idx 컬럼값은 max+1과 같아야 한다.")
+	@Test				
 	void insertMain() {
 		Community vo = Community.builder()
 				.writer("트트트")
 				.title("자바정석 후기")
 				.content("자바 공부하기 자바 정석으로")
 				.build();
+		int result = communityDao.maxOf();
 		communityDao.insert(vo);
-		assertEquals(vo.getIdx(), 11);    //기대값 : 생성된 시퀀스값
+		assertEquals(vo.getIdx(), result+1);    //기대값 : 생성된 시퀀스값
+	}
+	
+	@DisplayName("메인글 목록 : list 크기와 count 로 구한 값이 같아야 한다.")
+	@Test
+	void mainList() {
+		List<Community> list =	communityDao.list();
+		int count = communityDao.count();
+		assertEquals(list.size(), count);    //기대값 : 생성된 시퀀스값
 	}
 
 }
