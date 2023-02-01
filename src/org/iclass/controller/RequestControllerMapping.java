be2f5cf11c63bc7b164960b71767ffc7bff49902
kimsewhee <koreait.2021@gmail.com>
2023-02-01 23:12:24 +0900

    #11 URL추가 -글 읽기/수정/삭제

--- src/org/iclass/controller/RequestControllerMapping.java
+++ src/org/iclass/controller/RequestControllerMapping.java
@@ -3,7 +3,11 @@ package org.iclass.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.iclass.controller.community.DeleteController;
 import org.iclass.controller.community.ListController;
+import org.iclass.controller.community.ReadController;
+import org.iclass.controller.community.UpdateController;
+import org.iclass.controller.community.UpdateViewController;
 import org.iclass.controller.community.WriteController;
 import org.iclass.controller.community.WriteViewController;
 
@@ -15,6 +19,11 @@ public class RequestControllerMapping {
 		mapping.put(new RequestKeyValue("/community/list", "GET"), new ListController() );
 		mapping.put(new RequestKeyValue("/community/write","GET"), new WriteViewController() );
 		mapping.put(new RequestKeyValue("/community/write","POST"), new WriteController() );
+		//게시판 글 상세보기 ,수정하기,삭제하기
+		mapping.put(new RequestKeyValue("/community/read", "GET"), new ReadController() );
+		mapping.put(new RequestKeyValue("/community/update","GET"), new UpdateViewController() );
+		mapping.put(new RequestKeyValue("/community/update","POST"), new UpdateController() );
+		mapping.put(new RequestKeyValue("/community/delete","GET"), new DeleteController() );
 		//회원가입
 		mapping.put(new RequestKeyValue("/member/join","GET"), null);
 		mapping.put(new RequestKeyValue("/member/join","POST"), null);
