fda793e173045d03f09ccd677e2d980c7dc76f8a
kimsewhee <koreait.sec2020@gmail.com>
2023-02-02 15:03:41 +0900

    #11 댓글 목록 가져오기

--- src/org/iclass/controller/community/ReadController.java
+++ src/org/iclass/controller/community/ReadController.java
@@ -1,6 +1,7 @@
 package org.iclass.controller.community;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.iclass.controller.Controller;
 import org.iclass.dao.CommunityDao;
 import org.iclass.vo.Community;
+import org.iclass.vo.CommunityComments;
 
 // 요청 매핑 : mapping.put(new RequestKeyValue("/community/read", "GET"), new ReadController() );
 public class ReadController implements Controller {
@@ -26,10 +28,15 @@ public class ReadController implements Controller {
 		}
 		
 		CommunityDao dao = CommunityDao.getInstance();
+		//조회수 증가 : 실행 위치는?
+		dao.setReadCount(idx);
+		
 		Community vo = dao.selectByIdx(idx);
 		request.setAttribute("vo", vo);
 		
 		//idx 메인글의 댓글리스트를 애트리뷰트에 저장하기 해보세요.
+		List<CommunityComments> cmtlist = dao.comments(idx);
+		request.setAttribute("cmtlist", cmtlist);
 		
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("read.jsp");
