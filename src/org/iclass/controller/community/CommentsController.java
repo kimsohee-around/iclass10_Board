11adc22a59c4ac8214ea64e776a315f00a981385
kimsewhee <koreait.2021@gmail.com>
2023-02-02 19:43:53 +0900

    #12 url 매핑 - /community/comments

---
+++ src/org/iclass/controller/community/CommentsController.java
@@ -0,0 +1,22 @@
+package org.iclass.controller.community;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.iclass.controller.Controller;
+
+public class CommentsController implements Controller {
+	
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 댓글 작성 추가
+		
+		
+		//댓글 삭제
+		
+	}
+
+}
